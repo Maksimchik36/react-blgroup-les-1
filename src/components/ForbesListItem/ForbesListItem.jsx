d07ac4f7afb097a36963837b888cb540e353a46e
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-07-03 13:59:09 +0300

    add_task3_vovaK

--- src/components/ForbesListItem/ForbesListItem.jsx
+++ src/components/ForbesListItem/ForbesListItem.jsx
@@ -4,6 +4,15 @@ import { ForbesItem, Avatar, Name, Capital } from './ForbesListItem.styled';
 
 import { theme } from 'styles/theme';
 
-export const ForbesListItem = () => {
-  return <div>ForbesItem</div>;
+export const ForbesListItem = ({ list }) => {
+  return (
+    <ForbesItem>
+      <Avatar src={list.avatar} alt="{list.name}" />
+      <Name>{list.name}</Name>
+      <Capital>
+        {list.capital} <BiDollarCircle color={theme.colors.accent} size={22} />
+        {list.isIncrease === true ? FcBullish : FcBearish}
+      </Capital>
+    </ForbesItem>
+  );
 };
